5dd82f93e50e8c593e122f03e560ba95e823e12e
Kgosiaone <Motheomatsha077@gmail.com>
2025-07-08 08:49:10 +0200

    Add files via upload

---
+++ python.py
@@ -0,0 +1,13 @@
+#Operators
+
+# Addition (+)
+# Substraction (-)
+# Multiplication (*)
+# Division (/)
+# Modulu (%)
+# Exponent (**)
+
+x = 10
+x += 2
+
+print(x)
---
+++ shopping_cart.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+food = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter a food to buy or press q to quit")
+    if food == 'q':
+        break
+    else:
+        price = float(input(f"Entet yje price of the food"): R")
+        foods.append(food)              
+        foods.append(price)
+
+print("-----YOUR CART-----")    
+
+for food in foods:
+print 
